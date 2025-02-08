cf1e2587da300058bb2f7e6ac11bf45d66790c3e
EGJ-Moorington <gerlaitz@gmail.com>
2025-02-09 00:39:03 +0100

    Changed display SPI port to leave faster port with FIFO for radio

--- src/main.cpp
+++ src/main.cpp
@@ -3,26 +3,15 @@
 #include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
 #include <SPI.h>
 
-#define TFT_CS 10
+#define TFT_CS -1 // CS is grounded
 #define TFT_RST 9 // Or set to -1 and connect to Arduino RESET pin
 #define TFT_DC 8
 
 // OPTION 1 (recommended) is to use the HARDWARE SPI pins, which are unique
-// to each board and not reassignable. For Arduino Uno: MOSI = pin 11 and
-// SCLK = pin 13. This is the fastest mode of operation and is required if
-// using the breakout board's microSD card.
+// to each board and not reassignable.
 
 // For 1.14", 1.3", 1.54", 1.69", and 2.0" TFT with ST7789:
-Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
-
-// OPTION 2 lets you interface the display using ANY TWO or THREE PINS,
-// tradeoff being that performance is not as fast as hardware SPI above.
-// #define TFT_MOSI 11  // Data out
-// #define TFT_SCLK 13  // Clock out
-
-// OR for the ST7789-based displays, we will use this call
-// Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK,
-// TFT_RST);
+Adafruit_ST7789 tft = Adafruit_ST7789(&SPI1, TFT_CS, TFT_DC, TFT_RST);
 
 float p = 3.1415926;
 
